7313dd7e186b7b12eb6940bf610d3b280c67b6cd
Alexandr Romanov <rmnff.dev@yandex.ru>
2018-11-05 17:40:25 +0300

    gzip implemented

--- cipher_v2.go
+++ cipher_v2.go
@@ -1,13 +1,15 @@
 package api
 
 import (
-	"fmt"
+	"bytes"
+	"compress/gzip"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	random "math/rand"
@@ -17,7 +19,25 @@ import (
 	"github.com/pierrec/lz4"
 )
 
-func CompressData(ssource string) []byte {
+func CompressSmallData(source string) []byte {
+	data := []byte(source)
+	buf := &bytes.Buffer{}
+	gw := gzip.NewWriter(buf)
+	gw.Write(data)
+	gw.Close()
+	return buf.Bytes()
+}
+
+func DecompressSmallData(source []byte) string {
+	var buf bytes.Buffer
+	gr, _ := gzip.NewReader(bytes.NewBuffer(source))
+	defer gr.Close()
+	source, _ = ioutil.ReadAll(gr)
+	buf.Write(source)
+	return buf.String()
+}
+
+func CompressBigData(ssource string) []byte {
 	source := []byte(ssource)
 	compressed := make([]byte, len(source))
 	_, err := lz4.CompressBlockHC(source, compressed, 0)
@@ -31,7 +51,7 @@ func CompressData(ssource string) []byte {
 	return compressed
 }
 
-func DecompressData(source []byte) string {
+func DecompressBigData(source []byte) string {
 	decompressed := make([]byte, len(source) * 10)
 	_, err := lz4.UncompressBlock(source, decompressed)
 	if err != nil {
