ef01fdf685fe6300e6c957d0c161d7f0fe7d5df6
Alexandr Romanov <rmnff.dev@yandex.ru>
2019-02-01 00:24:54 +0300

    fresh updates

--- ipfs_api.go
+++ ipfs_api.go
@@ -1,13 +1,11 @@
 package api
 
 import(
-	"encoding/json"
 	"fmt"
-	"bytes"
+	"bufio"
 	"os"
-	"io"
-	"mime/multipart"
-	"strings"
+
+	shell "github.com/ipfs/go-ipfs-api"
 )
 
 type ipfsResponse struct {
@@ -16,8 +14,30 @@ type ipfsResponse struct {
 	Size string `json:"Size"`
 }
 
+func (c *Commander) AddFileToIPFS(filepath string) string {
+	sh := shell.NewShell("https://ipfs.infura.io:5001")
+	f, err := os.Open(filepath)
+	buf := bufio.NewReader(f)
+	cid, err := sh.Add(buf)
+	if err != nil {
+		return ""
+	}
+	return cid
+}
+
+func (c *Commander) HashFileIPFS(filepath string) string {
+	sh := shell.NewShell("https://ipfs.infura.io:5001")
+	opts := shell.OnlyHash(true)
+	f, err := os.Open(filepath)
+	buf := bufio.NewReader(f)
+	cid, err := sh.Add(buf, opts)
+	if err != nil {
+		return ""
+	}
+	return cid
+}
+
 const URLCat = "ipfs.infura.io:5001/api/v0/cat"
-const URLAdd = "ipfs.infura.io:5001/api/v0/add?pin=false"
 
 func (c *Commander) CatFileFromIPFS(hash string) ([]byte, error) {
 	url := fmt.Sprintf("%s?arg=%s", URLCat, hash)
@@ -26,51 +46,4 @@ func (c *Commander) CatFileFromIPFS(hash string) ([]byte, error) {
 		return []byte{}, err
 	}
 	return []byte(data), nil
-}
-
-func (c *Commander) AddFileToIPFS(filepath string) string {
-	split := strings.Split(filepath, "/")
-	filename := split[len(split) - 1]
-	split = nil
-	bodyBuf := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuf)
-	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-	if err != nil {
-		fmt.Println("GOLANG")
-		fmt.Println("error writing to buffer")
-		fmt.Println(err)
-		return ""
-	}
-	fh, err := os.Open(filepath)
-	if err != nil {
-		fmt.Println("GOLANG")
-		fmt.Println("error opening file")
-		fmt.Println(err)
-		return ""
-	}
-	defer fh.Close()
-	_, err = io.Copy(fileWriter, fh)
-	if err != nil {
-		fmt.Println("GOLANG")
-		fmt.Println(err)
-		return ""
-	}
-	contentType := bodyWriter.FormDataContentType()
-	bodyWriter.Close()
-	// this operation might be necessary later on
-	// os.Remove(filepath)
-	data, err := RequestPostHTTPS(URLAdd, contentType, bodyBuf)
-	if err != nil {
-		fmt.Println("GOLANG")
-		fmt.Println(err)
-		return ""
-	}
-	response := &ipfsResponse{}
-	err = json.Unmarshal([]byte(data), response)
-	if err != nil {
-		fmt.Println("GOLANG")
-		fmt.Println(err)
-		return ""
-	}
-	return response.Hash
 }
